207add9ef60c64aaeeca927ec4f277c0d226e8a2
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-14 10:30:46 -0700

    Task 6 completed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- * 
+ * main - Entry point of the program 
  * Description of a C prgogram to print using printf
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
