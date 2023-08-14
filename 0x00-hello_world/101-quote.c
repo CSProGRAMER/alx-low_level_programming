e41d9a64e8c9ce9d5611929001303cfef266d781
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-14 12:30:41 -0700

    101-quote.c done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,11 @@
 #include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints using puts function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
