c1740955ae5b3680f8554804fc57a8a90e43dbcf
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-14 10:24:35 -0700

    File updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  *
  * Description: a C program that prints using puts function
  *
- * Return: Always 0 (Success)    
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
