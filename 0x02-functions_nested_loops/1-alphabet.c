de205b656eccf626065dfb6da1d7e7bff4f8d7d0
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-15 16:07:06 -0700

    Task 2 done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,9 +5,10 @@
  * Description: a C program tha using putchar function
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)/* function print aplhabet */
+void print_alphabet(void) /* function print aplhabet */
 {
 	char letter = 'a';	
+	
 	while (letter <= 'z')
 	{
 		_putchar(letter);
