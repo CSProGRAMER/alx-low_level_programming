9de24aaa9c429d6ed2acb15b6f40ec9b1679c507
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-15 16:12:54 -0700

    Task 2 done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "_putchar.h"
-
 /**
- * main - Entry point
- * Description: a C program tha using putchar function
- * Return: Always 0 (Success)
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
+ *
+ * Description: This function prints the lowercase alphabet starting from 'a'
+ * and ending at 'z', followed by a new line character.
  */
-void print_alphabet(void) /* function print aplhabet */
+void print_alphabet(void)
 {
-	char letter = 'a';
+	char letter = 'a';/* Initialize the starting letter as 'a' */
 
 	while (letter <= 'z')
 	{
-		_putchar(letter);
+		_putchar(letter); /* Call _putchar to print the current letter */
 		letter++;
 	}
-	_putchar('\n');
+	_putchar('\n');/* Print a new line character after the alphabet */
 }
