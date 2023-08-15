0a1b0e4ed2cf71ea5e9ad34fc0e39b46fa3bea13
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-15 15:00:04 -0700

    task 1 done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
 /**
-main - Entry point
-
-Description: a C program that prints using puts function
-
-Return: Always 0 (Success)
-*/
+ * main - Entry poin
+ * Description: a C program that prints using puts function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  write(1, "_putchar\n", 9);
-  return (0);
+	write(1, "_putchar\n", 9);
+	return (0);
 }
