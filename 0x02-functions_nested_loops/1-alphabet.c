8c58eba5cc2d0a3642d65913b9916e46fd155279
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-15 15:08:32 -0700

    Task 2 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: a C program that prints using puts function
+ * Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
