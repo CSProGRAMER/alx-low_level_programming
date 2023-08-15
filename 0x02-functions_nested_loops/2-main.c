3bd5d91f5ba64c3ccc7fa197da16aecdd269bbd1
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-15 16:34:51 -0700

    Task 3 Done

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+void print_alphabet_x10(void);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "_putchar.h"
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times, followed by new lines.
+ *
+ * Description: This function prints the lowercase alphabet starting from 'a'
+ * and ending at 'z', repeated 10 times. Each repetition is followed
+ * by a new line character.
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a'; /* Initialize the starting letter as 'a' */
+		while (letter <= 'z')
+		{
+			_putchar(letter); /* Call _putchar to print the current letter */
+			letter++;
+		}
+		_putchar('\n'); /* Print a new line character after each repetition */
+	}
+}
