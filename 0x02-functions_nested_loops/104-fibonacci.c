486e65359d2ee2559ed0aa3309c6f6ddc7f0cbb0
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-16 09:07:15 -0700

    TASK 104 IS DONE

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int a = 1, b = 2, c;
+	int count;
+
+	printf("%lu, %lu", a, b);
+
+	for (count = 2; count < 98; count++)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+	}
+
+	printf("\n");
+	return (0);
+}
