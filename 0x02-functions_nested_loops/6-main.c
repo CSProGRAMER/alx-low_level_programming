9302f1beeb479f922fcbc1658fa8c121bc204679
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-16 07:32:05 -0700

    Task 6 done

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+    int r;
+
+    r = _abs(-98);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1024);
+    printf("%d\n", r);
+    r = _abs(-1024);
+    printf("%d\n", r);
+
+    return (0);
+}
+
