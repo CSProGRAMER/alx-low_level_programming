40e0890cd27052436e3113f8e54790b61d110916
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-15 17:13:10 -0700

    Task5 edited

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -6,16 +6,12 @@ int main(void)
     int r;
 
     r = print_sign(98);
-    putchar('\n');
     printf("%d\n", r);
     r = print_sign(0);
-    putchar('\n');
     printf("%d\n", r);
     r = print_sign(0xff);
-    putchar('\n');
     printf("%d\n", r);
     r = print_sign(-1);
-    putchar('\n');
     printf("%d\n", r);
     return (0);
 }
