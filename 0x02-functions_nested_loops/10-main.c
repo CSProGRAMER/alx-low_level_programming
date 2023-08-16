8ca1e1ab0baceed0c2c6dbe11b4423f55a2d461c
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-16 08:17:09 -0700

    Task 10 Is DONE

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int result;
+
+    result = add(5, 10);
+    printf("Result: %d\n", result);
+
+    return (0);
+}
+
