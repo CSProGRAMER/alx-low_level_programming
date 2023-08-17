70f912c168050cf07f83e8cb7ac48629dfb2b341
CSProGRAMER <mahmed.safwt@gmail.com>
2023-08-17 09:51:52 -0700

    Task 6 is done

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,13 @@
+#include "_putchar.h"
+#include "main.h"
+int _putchar(char c);
+void print_line(int n);
+
+int main(void)
+{
+    print_line(5);
+    print_line(0);
+    print_line(-2);
+    return (0);
+}
+
