9bed2b04b84fc5eee89f9a1a8685325c0c3ec9fb
CSProGRAMER <mahmed.safwt@gmail.com>
2023-09-04 20:27:01 +0300

    file edited

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,5 +14,5 @@ int _pow_recursion(int x, int y)
 	else if (y == 0)
 		return (1); /* Base case: x^0 is always 1 */
 	else
-		return (x * _pow_recursion(x, y - 1)); /* Recursive case: x^y = x * x^(y-1) */
+		return (x * _pow_recursion(x, y - 1));
 }
