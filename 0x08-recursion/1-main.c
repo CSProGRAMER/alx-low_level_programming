7fa4fd4e129c468f68674750ee9b29f9c4606300
root <root@DESKTOP-79N2301>
2023-09-04 19:42:48 +0300

    Task 2 is done

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(void)
+{
+	    char *str = "Reverse Me!";
+
+	        _print_rev_recursion(str);
+		    _putchar('\n'); /* Print a newline after the reversed string */
+
+		        return (0);
+}
+
