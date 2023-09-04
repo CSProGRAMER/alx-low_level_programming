353d85d91d85b1371b5f0228cccb43f27f1e69ec
CSProGRAMER <mahmed.safwt@gmail.com>
2023-09-04 22:19:48 +0300

    task 3 is done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,7 +4,6 @@
  * main - Entry point of the program
  * @argc: The number of command-line arguments
  * @argv: An array containing the command-line arguments
- * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
