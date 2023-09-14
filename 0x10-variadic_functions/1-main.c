0cee41e926bfc97b623f5eec0d2cc66aba7ecd9d
CSProGRAMER <mahmed.safwt@gmail.com>
2023-09-15 02:48:43 +0300

    Task 2 is done

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+int main(void)
+{
+		print_numbers(", ", 4, 1, 2, 3, 4);
+			print_numbers(", ", 0);
+				print_numbers(NULL, 3, 10, 20, 30);
+					return (0);
+}
+
