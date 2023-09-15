7260773983af685ab88e4aee0b6dc18411947b45
CSProGRAMER <mahmed.safwt@gmail.com>
2023-09-15 03:15:21 +0300

    file edited

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,6 +5,7 @@
  * print_name - Prints a name.
  * @name: The name to be printed.
  * @f: A function pointer that specifies how to print the name.
+ * @*: its about
  */
 void print_name(char *name, void (*f)(char *))
 {
