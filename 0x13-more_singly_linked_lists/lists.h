401fd09266ffcdf497b3c0a34368a63bd2d235a8
CSProGRAMER <mahmed.safwt@gmail.com>
2023-09-27 01:24:50 +0300

    task 1 is done

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/* Structure for singly linked list of integers */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
+size_t print_listint(const listint_t *h);
+
+#endif /* LISTS_H */
+
